c493cfdd54cd67b4ffe57f5db83f90031ee0e689
kuldeepsinghlive@gmail.com <kuldeepsinghlive@gmail.com>
2020-02-08 00:25:40 -0700

    Fix for MultiLSTM

--- iep/models/seq2seq.py
+++ iep/models/seq2seq.py
@@ -33,8 +33,10 @@ class Seq2Seq(tf.Module):
                  ):
 
         self.encoder_embed = tf.keras.layers.Embedding(encoder_vocab_size, wordvec_dim)
-        self.encoder_rnn = tf.keras.layers.LSTM(wordvec_dim, hidden_dim, rnn_num_layers,
-                                                dropout=rnn_dropout, batch_first=True)
+        cells = [tf.keras.layers.LSTMCell(hidden_dim, dropout=rnn_dropout) for _ in range(rnn_num_layers)]
+        self.encoder_rnn = tf.keras.layers.StackedRNNCells(cells)
+        #    tf.keras.layers.LSTM(wordvec_dim, hidden_dim, rnn_num_layers,
+        #                                        dropout=rnn_dropout, batch_first=True)
         self.decoder_embed = tf.keras.layers.Embedding(decoder_vocab_size, wordvec_dim)
         self.decoder_rnn = tf.keras.layers.LSTM(wordvec_dim + hidden_dim, hidden_dim, rnn_num_layers,
                                                 dropout=rnn_dropout, batch_first=True)
